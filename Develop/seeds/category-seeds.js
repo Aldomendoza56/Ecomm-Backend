86902cec1d568336852814e907ea9f95f38b9347
Aldo Mendoza <aldomendoza56@gmail.com>
2024-03-02 14:23:30 -0800

    added routes

--- Develop/seeds/category-seeds.js
+++ Develop/seeds/category-seeds.js
@@ -1,23 +1,21 @@
 const { Category } = require('../models');
 
 const categoryData = [
-  {
-    category_name: 'Shirts',
-  },
-  {
-    category_name: 'Shorts',
-  },
-  {
-    category_name: 'Music',
-  },
-  {
-    category_name: 'Hats',
-  },
-  {
-    category_name: 'Shoes',
-  },
+  { category_name: 'Shirts' },
+  { category_name: 'Shorts' },
+  { category_name: 'Music' },
+  { category_name: 'Hats' },
+  { category_name: 'Shoes' }
 ];
 
-const seedCategories = () => Category.bulkCreate(categoryData);
+const seedCategories = async () => {
+  try {
+    await Category.sync(); // Create the Category table if it doesn't exist
+    await Category.bulkCreate(categoryData);
+    console.log('Category seed data inserted!');
+  } catch (err) {
+    console.error('Error inserting category seed data:', err);
+  }
+};
 
 module.exports = seedCategories;
